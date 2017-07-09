020559e97c1dd293eb416ed130914de4753578a3
administrator <administrator@korea.ac.kr>
2017-07-10 01:40:20 +0900

    Debug previous commit

--- NewbieSimulator/NewbieSimulator/fsm.c
+++ NewbieSimulator/NewbieSimulator/fsm.c
@@ -1,14 +1,7 @@
 #include "core.h"
 #include "engine.h"
-<<<<<<< Updated upstream:NewbieSimulator/NewbieSimulator/fsm.c
-#include "fsms.h"
-=======
-<<<<<<< Updated upstream:NewbieSimulator/NewbieSimulatorEngine/fsm.c
-=======
 #include "fsms.h"
  
->>>>>>> Stashed changes:NewbieSimulator/NewbieSimulator/fsm.c
->>>>>>> Stashed changes:NewbieSimulator/NewbieSimulatorEngine/fsm.c
 
 //----------------------------------------
 // Scene 0
@@ -29,28 +22,8 @@ int action_0() {
 int late_update_0() {
 
 	//action이 끝났을 때.
-	
-<<<<<<< Updated upstream:NewbieSimulator/NewbieSimulator/fsm.c
-	int clicked = 0;
 
-	if (ev.type == ALLEGRO_EVENT_MOUSE_BUTTON_DOWN) {
-		//transition
-		// if mouse clicked, change scene 0 --> 1
-		clicked++;
-
-		if (clicked == 1) {
-			transition_0_to_1();
-		}
-	}
-=======
-<<<<<<< Updated upstream:NewbieSimulator/NewbieSimulatorEngine/fsm.c
-	if (ev.type == ALLEGRO_EVENT_MOUSE_BUTTON_DOWN) {
-		//transition
-		// if mouse clicked, change scene 0 --> 1
-		transition_0_to_1();
-=======
 	static int down = 0;	
->>>>>>> Stashed changes:NewbieSimulator/NewbieSimulatorEngine/fsm.c
 
 	if ( catch_event().type == ALLEGRO_EVENT_MOUSE_BUTTON_DOWN) {
 		if (!down) {
@@ -63,7 +36,6 @@ int late_update_0() {
 			transition_0_to_1();
 			down = 0;
 		}
->>>>>>> Stashed changes:NewbieSimulator/NewbieSimulator/fsm.c
 	}
 	
 	return 0;
@@ -73,21 +45,11 @@ int transition_0_to_1() {
 
 	// state_000에서 state_001로의 전이.
 
-	printf("mouse click \n");
-<<<<<<< Updated upstream:NewbieSimulator/NewbieSimulator/fsm.c
-	load_scene(Scenes.scenes[1]);
-=======
-<<<<<<< Updated upstream:NewbieSimulator/NewbieSimulatorEngine/fsm.c
-	current = Scenes.scenes[1];
-=======
-
 	//다음 frame에 1번째 state를 로드
 	next = FSMs.states[1];
 
 	//다음 scene을 로드
 	load_scene(Scenes.scenes[1]);
->>>>>>> Stashed changes:NewbieSimulator/NewbieSimulator/fsm.c
->>>>>>> Stashed changes:NewbieSimulator/NewbieSimulatorEngine/fsm.c
 
 	return 0;
 }
