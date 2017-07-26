186e864c02830c7f7b7518cc3ae5d0555b88ff85
YangHun <pitahano@gmail.com>
2017-07-26 20:27:26 +0900

    Add some example buttons

--- NewbieSimulator/NewbieSimulator/scene_2.c
+++ NewbieSimulator/NewbieSimulator/scene_2.c
@@ -14,20 +14,58 @@ int analyzeMessage = MESSAGE_DEFAULT;
 schedule mySchedule;
 schedule* mySchedulePtr = &mySchedule;
 
+ALLEGRO_CONFIG *conf;
+
 static void on_click_button_0();
+static void on_click_button_selective();
+static void on_click_button_major();
+static void on_click_button_core();
 
 int scene_2_init() {
 	//해당 씬이 시작될 때, 딱 한 번 실행되는 함수
 	system("chcp 65001");
 	printf("Scene 2 start!");
 
-	object_t bg = create_object("Resources\\dummy\\tutorial.jpg", 0, 0);
+	object_t bg = create_object("Resources\\UI\\enroll\\background.jpg", 0, 0);
 	Background = bg;
 
-	object_t hos = create_object("Resources\\dummy\\hos.png", 100, 100);
-	ui_set_button(&hos);
-	ui_set_on_click_listener(&hos, on_click_button_0);
-	Stack.push(&Stack, hos);
+	conf = al_load_config_file("Resources\\korean\\enroll.ini");
+
+
+	//------------------------------------------------
+	// Buttons
+	//------------------------------------------------
+
+	object_t button_selective = create_object("Resources\\UI\\enroll\\b_select_normal.png", 0, 34);
+	ui_set_button(&button_selective);
+	ui_set_on_click_listener(&button_selective, on_click_button_selective);
+	Stack.push(&Stack, button_selective);
+#define BUTTON_SELECTIVE Stack.objs[0]
+	
+	object_t button_major = create_object("Resources\\UI\\enroll\\b_major_normal.png", 0, 157);
+	ui_set_button(&button_major);
+	ui_set_on_click_listener(&button_major, on_click_button_major);
+	Stack.push(&Stack, button_major);
+#define BUTTON_MAJOR Stack.objs[1]
+
+	object_t button_core = create_object("Resources\\UI\\enroll\\b_core_normal.png", 0, 280);
+	ui_set_button(&button_core);
+	ui_set_on_click_listener(&button_selective, on_click_button_core);
+	Stack.push(&Stack, button_core);
+#define BUTTON_CORE Stack.objs[2]
+
+	//------------------------------------------------
+	// Texts
+	//------------------------------------------------
+
+	object_t n = create_object(NULL, 1015, 567);
+	ui_set_text(&n, al_map_rgb(0, 0, 0), "Resources\\font\\NanumGothic.ttf", ALLEGRO_ALIGN_LEFT, "7", 24);
+	Stack.push(&Stack, n);
+
+	object_t number = create_object(NULL, 1026, 567);
+	ui_set_text(&number, al_map_rgb(0, 0, 0), "Resources\\font\\NanumGothic.ttf", ALLEGRO_ALIGN_LEFT, al_get_config_value(conf, "korean", "number"), 24);
+	Stack.push(&Stack, number);
+
 
 	xmlParse(lectureTable);
 	init_mySchedule(mySchedulePtr);
@@ -88,6 +126,21 @@ int scene_2_fin() {
 
 void on_click_button_0() {
 	load_scene(Scenes.scenes[1]);
+
+}
+
+void on_click_button_selective() {
+	//do it!
+
+}
+
+void on_click_button_major() {
+	//do it!
+
+}
+
+void on_click_button_core() {
+	//do it!
 
 }
 
